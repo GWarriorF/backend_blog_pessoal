5aa7f46ec05085992b8722db201330a00d19d04c
Gabriela Guerra <gabrielaguerra438@gmail.com>
2023-06-27 15:36:37 -0300

    @GabrielaFigueiredoDEV do molde do objeto tema, repositótio do tema e seus respectivos métodos CRUD

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -26,6 +28,10 @@ public class Postagem
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema; 
 
 	public Long getId() {
 		return id;
@@ -58,4 +64,13 @@ public class Postagem
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+		
 }
